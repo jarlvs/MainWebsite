0e13b6847dca38adbd4f77879d19d3fea8117cce
jarlvs <premshinde13@gmail.com>
2017-08-18 03:47:00 +0530

    working on campaigns

--- js/campaigns.js
+++ js/campaigns.js
@@ -3,8 +3,9 @@
     var app = angular.module('campaigns', []);
     app.controller('campaignsCtrl', function($scope) {
     $scope.volunteerData= "Volunteer weekends aim to enlighten the youth by making them conscious of the social issues and encouraging them to initiate social change. Under this initiative, we conduct visits to various NGOs, Oldage Homes, Orphanages etc., interact with them and deliver our services.";
-    $scope.mastiKiPaathshaalaData = "It is said “When you enjoy what you do, work becomes play”. The “Masti ki Paathshaala” initiative aims to teach science to children through a more innovative and fascinating way with the help of fun experiments and toys!";
+    $scope.mastiKiPaathshaalaData = "It is said “When you enjoy what you do, work becomes play”. The “Masti ki Paathshaala” initiative aims to teach science to children through a more innovative and fascinating way with the help of fun experiments and toys! We love you a lot guys";
     $scope.careerData="The Career Counselling Campaign provides free career guidance to underprivileged students. We reach out to the students through sessions conducted in various schools of Mumbai and through our app called the 'Career Counselling Helpline'.";
+    $scope.tab=1;
     });
 
 
